76033ecdb92f178be891616e041878e8a518bc55
Bryce <77906727+nice-bryce@users.noreply.github.com>
2025-09-26 17:00:34 +1000

    Almost working

---
+++ common/defines/totalmobammunitiondefines.lua
@@ -0,0 +1,4 @@
+-- Adjusts experience factor
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 10000
+NDefines.NMilitary.FIELD_EXPERIENCE_SCALE = 0.0694
+NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY = 5000	
